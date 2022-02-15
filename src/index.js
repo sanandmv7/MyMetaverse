9e888bd1d283a0c4735ac297551e9db9cb864586
sanandmv7 <56066399+sanandmv7@users.noreply.github.com>
2022-02-15 19:52:12 +0530

    Add ground

--- src/index.js
+++ src/index.js
@@ -13,16 +13,20 @@ const dlight = new THREE.DirectionalLight(0xffffff, 0.5);
 light.add(dlight);
 scene.add(light);
 
-const geometry = new THREE.BoxGeometry();
+const geometry = new THREE.BoxGeometry(50, 0.1, 50);
 const material = new THREE.MeshPhongMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
 
-camera.position.z = 5;
+const ground = new THREE.Mesh( geometry, material );
+scene.add( ground );
+
+// const cube = new THREE.Mesh( geometry, material );
+// scene.add( cube );
+
+camera.position.set(5, 15, 15);
 
 function animate() {
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+    // cube.rotation.x += 0.01;
+    // cube.rotation.y += 0.01;
 	requestAnimationFrame( animate );
 	renderer.render( scene, camera );
 }
