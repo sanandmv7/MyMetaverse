481ad1ab61512c128828eb01d50f03eee92622b7
sanandmv7 <56066399+sanandmv7@users.noreply.github.com>
2022-02-15 19:43:16 +0530

    Animate the cube

--- src/index.js
+++ src/index.js
@@ -14,4 +14,10 @@ scene.add( cube );
 
 camera.position.z = 5;
 
-renderer.render(scene, camera);
+function animate() {
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+	requestAnimationFrame( animate );
+	renderer.render( scene, camera );
+}
+animate();
